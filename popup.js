f2cb0ea96f1c6b2427884d218695cc4ca815bce9
Anand Shukla <anandshukla.web@gmail.com>
2022-12-05 17:04:43 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector(".changeColorBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
+
+btn.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickColor,
+    },
+    async (injectionResults) => {
+      const [data] = injectionResults;
+      if (data.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerText = color;
+
+        try {
+          await navigator.clipboard.writeText(color);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  );
+});
+
+async function pickColor() {
+  try {
+    // Picker
+    const eyeDropper = new EyeDropper();
+
+    return await eyeDropper.open();
+  } catch (error) {
+    console.error(error);
+  }
+}
